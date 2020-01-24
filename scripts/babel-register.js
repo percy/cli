7f72ae6be75faec490f2fbb53a78bcfe4c4e8b83
Wil Wilsman <wil@wilwilsman.com>
2020-01-24 17:25:23 -0600

    ➕ Add babel, eslint, and istanbul

---
+++ scripts/babel-register.js
@@ -0,0 +1,15 @@
+const path = require('path')
+
+require('@babel/register')({
+  // allow monorepos to share a single babel config
+  rootMode: 'upward',
+
+  // specified without the cwd so tests can share helpers
+  only: [
+    new RegExp(
+      ['packages', '.*?', '(src|test)']
+      // escape windows path seperators and escape the escape
+        .join(path.sep === '/' ? '/' : '\\\\')
+    )
+  ]
+})
