966c570f743719ecab183b82c7e68f06d31c15b1
Wil Wilsman <wil@wilwilsman.com>
2020-02-24 17:25:44 -0600

    ✨ Add @percy/cli package

---
+++ packages/cli/README.md
@@ -0,0 +1,6 @@
+# @percy/cli
+
+A collection of CLI commmands for taking Percy snapshots.
+
+## Commands
+<!-- commands -->
---
+++ packages/cli/index.js
@@ -0,0 +1,2 @@
+// This package is a multi-command oclif CLI composed of plugins that provide
+// various functionalities for taking Percy snapshots.
