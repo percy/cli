43d17082648c9142455bab5277cf75474064cfc3
Wil Wilsman <wil@wilwilsman.com>
2020-04-24 23:00:38 -0500

    🏷️ Add @percy/core types

---
+++ packages/core/types/index.d.ts
@@ -0,0 +1,69 @@
+// Minimum TypeScript Version: 3.8
+import 'puppeteer-core/lib/externs';
+
+type LogLevel = 'error' | 'warn' | 'info' | 'debug' | 'silent';
+interface Pojo { [x: string]: any; }
+
+export interface SnapshotOptions {
+  widths?: number[];
+  minimumHeight?: number;
+  percyCSS?: string;
+  requestHeaders?: Pojo;
+  enableJavaScript?: boolean;
+}
+
+interface DiscoveryOptions {
+  allowedHostnames?: string[];
+  networkIdleTimeout?: number;
+  disableAssetCache?: boolean;
+  concurrency?: number;
+  launchOptions?: Pojo;
+}
+
+export type PercyOptions<C = Pojo> = C & {
+  token?: string,
+  clientInfo?: string,
+  environmentInfo?: string,
+  server?: boolean,
+  port?: number,
+  concurrency?: number,
+  loglevel?: LogLevel,
+  snapshot?: SnapshotOptions,
+  discovery?: DiscoveryOptions
+};
+
+type CaptureExec = (page: Puppeteer.Page) => Promise<void>;
+type CaptureSnapshots = Array<{ name: string, execute: CaptureExec }>;
+
+declare class Percy {
+  static start(options?: PercyOptions): Promise<Percy>;
+  constructor(options?: PercyOptions);
+  loglevel(): LogLevel;
+  loglevel(level: LogLevel): void;
+  isRunning(): boolean;
+  start(): Promise<void>;
+  stop(): Promise<void>;
+
+  snapshot(options: SnapshotOptions & {
+    url: string,
+    name: string,
+    domSnapshot: string,
+    clientInfo?: string,
+    environmentInfo?: string
+  }): Promise<void>;
+
+  capture(options: SnapshotOptions & ({
+    url: string,
+    name: string,
+    snapshots?: CaptureSnapshots,
+    waitFor?: string | number,
+    execute?: CaptureExec,
+  } | {
+    url: string,
+    snapshots: CaptureSnapshots,
+    waitFor?: string | number,
+    execute?: CaptureExec
+  })): Promise<void>;
+}
+
+export default Percy;
---
+++ packages/core/types/index.test-d.ts
@@ -0,0 +1,142 @@
+import { expectType, expectError } from 'tsd';
+import Percy, { PercyOptions } from '@percy/core';
+
+// PercyOptions
+const percyOptions: PercyOptions = {
+  token: 'PERCY_TOKEN',
+  clientInfo: 'sdk-client-info',
+  environmentInfo: 'sdk-environment-info',
+  server: false,
+  port: 5338,
+  concurrency: 1,
+  loglevel: 'info',
+  snapshot: {
+    widths: [1280],
+    minimumHeight: 1024,
+    percyCSS: '.percy { color: purple; }',
+    requestHeaders: { Authorization: 'foobar' },
+    enableJavaScript: false
+  },
+  discovery: {
+    allowedHostnames: ['*.percy.io'],
+    networkIdleTimeout: 100,
+    disableAssetCache: false,
+    concurrency: 1,
+    launchOptions: {
+      devtools: true
+    }
+  },
+  foo: {
+    bar: 'baz'
+  }
+};
+
+// new Percy()
+const percy = new Percy();
+expectType<Percy>(percy);
+expectType<Percy>(new Percy(percyOptions));
+// Percy.start()
+expectType<Percy>(await Percy.start());
+expectType<Percy>(await Percy.start(percyOptions));
+
+// #loglevel()
+expectType<'error' | 'warn' | 'info' | 'debug' | 'silent'>(percy.loglevel());
+expectType<void>(percy.loglevel('error'));
+
+// #isRunning()
+expectType<boolean>(percy.isRunning());
+
+// #start()
+expectType<void>(await percy.start());
+// #stop()
+expectType<void>(await percy.stop());
+
+// #snapshot()
+expectType<void>(await percy.snapshot({
+  name: 'test snapshot',
+  url: 'http://localhost:3000',
+  domSnapshot: '...',
+  widths: [1000],
+  minimumHeight: 1000,
+  percyCSS: '.foo { font-weight: 900; }',
+  requestHeaders: { 'x-testing': 'test' },
+  enableJavaScript: true
+}));
+
+expectType<void>(await percy.snapshot({
+  name: 'test snapshot',
+  url: 'http://localhost:3000',
+  domSnapshot: '...'
+}));
+
+expectError(await percy.snapshot({
+  name: 'test snapshot',
+  url: 'http://localhost:3000',
+  domSnapshot: '...',
+  foo: true
+}));
+
+expectError(await percy.snapshot({
+  name: 'test snapshot',
+  url: 'http://localhost:3000'
+}));
+
+expectError(await percy.snapshot({
+  name: 'test snapshot'
+}));
+
+expectError(await percy.snapshot());
+
+// #capture()
+expectType<void>(await percy.capture({
+  name: 'test snapshot',
+  url: 'http://localhost:3000',
+  waitFor: '.some-selector',
+  execute: async page => {},
+  snapshots: [{
+    name: 'test 2',
+    execute: async page => {}
+  }],
+  widths: [1000],
+  minimumHeight: 1000,
+  percyCSS: '.foo { font-weight: 900; }',
+  requestHeaders: { 'x-testing': 'test' },
+  enableJavaScript: true
+}));
+
+expectType<void>(await percy.capture({
+  name: 'test snapshot',
+  url: 'http://localhost:3000'
+}))
+
+expectType<void>(await percy.capture({
+  url: 'http://localhost:3000',
+  snapshots: [{
+    name: 'test snapshot',
+    execute: async page => {}
+  }]
+}))
+
+expectError(await percy.capture({
+  url: 'http://localhost:3000'
+}))
+
+expectError(await percy.capture({
+  url: 'http://localhost:3000',
+  snapshots: [{
+    execute: async page => {}
+  }]
+}))
+
+expectError(await percy.capture({
+  url: 'http://localhost:3000',
+  snapshots: [{
+    name: 'test snapshot'
+  }]
+}))
+
+expectError(await percy.capture({
+  name: 'test snapshot'
+}))
+
+expectError(await percy.capture())
