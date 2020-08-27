24f5536f33b5cd71d6e0d9852a307ac0bb47ca9e
Wil Wilsman <wil@wilwilsman.com>
2020-08-27 12:32:00 -0500

    🐛 Idle helper should wait for timeout before resolving
    Previously, the timeout argument was being used as an interval, which had a similar effect but
    ultimately was prone to resolving at the beginning of the timeout rather than the end on subsequent
    idle checks after the initial check.

--- packages/core/src/utils/idle.js
+++ packages/core/src/utils/idle.js
@@ -1,9 +1,14 @@
-// Returns a promise that resolves when the `count` function returns `0`. If
-// `count` returns non-zero, will check again after `timeout`. The `count`
-// function must eventually return `0` or the promise will never resolve.
-export default function idle(count, timeout = 10) {
-  return new Promise(resolve => (function check() {
-    if (count() === 0) resolve();
-    else setTimeout(check, timeout);
+// Returns a promise that resolves when the `count` function returns `0`. If `count` returns
+// non-zero, it will check again after `interval`. The `count` function must eventually return `0`
+// or the promise will never resolve. If a timeout is provided, the check will be run again before
+// resolving to ensure `count` still returns `0` after the timeout.
+export default function idle(count, timeout = 0, interval = 10) {
+  return new Promise(resolve => (function check(last) {
+    if (count() === 0) {
+      if (!timeout || last) resolve();
+      else setTimeout(check, timeout, true);
+    } else {
+      setTimeout(check, interval);
+    }
   })());
 }
