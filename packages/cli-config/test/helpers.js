b71880f6631c8bebf8cd7799ad8737a4c81432ce
Wil Wilsman <wil@wilwilsman.com>
2020-09-23 19:09:53 -0500

    ✅ Use import/export for cli-config test helpers

--- packages/cli-config/test/helpers.js
+++ packages/cli-config/test/helpers.js
@@ -1,8 +1,4 @@
 // required before fs is mocked by the config helper
-require('@oclif/command');
-
-// `export from` is hoisted, so just use require
-const { default: mockConfig, getMockConfig } = require('@percy/config/test/helper');
-const { default: stdio } = require('@percy/logger/test/helper');
-
-export { stdio, mockConfig, getMockConfig };
+import '@oclif/command';
+export { default as stdio } from '@percy/logger/test/helper';
+export { default as mockConfig, getMockConfig } from '@percy/config/test/helper';
