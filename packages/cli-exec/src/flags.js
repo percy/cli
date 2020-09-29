264ce12a78c579a38be9687afd832acbbc5588e8
Wil Wilsman <wil@wilwilsman.com>
2020-09-29 12:20:18 -0500

    💥 Rename port char
    This option is rarely used, and the existing char can be utilized elsewhere

--- packages/cli-exec/src/flags.js
+++ packages/cli-exec/src/flags.js
@@ -2,7 +2,7 @@ import { flags } from '@percy/cli-command';
 
 export default {
   port: flags.integer({
-    char: 'p',
+    char: 'P',
     description: 'server port',
     default: process.env.PERCY_CLI_PORT || 5338
   })
