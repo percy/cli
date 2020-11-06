4e865664bb1baf8a341e5d939487048191b01e29
Wil Wilsman <wil@wilwilsman.com>
2020-11-06 14:32:11 -0600

    ♻ Reuse normalize helper to merge overrides

--- packages/config/src/normalize.js
+++ packages/config/src/normalize.js
@@ -1,25 +1,5 @@
-// recursively reduces config objects and arrays to remove undefined and empty
-// values and rename kebab-case properties to camelCase.
-export default function normalize(subject) {
-  if (typeof subject === 'object') {
-    let isArray = Array.isArray(subject);
-
-    return Object.entries(subject)
-      .reduce((result, [key, value]) => {
-        value = normalize(value);
-
-        if (typeof value !== 'undefined') {
-          return isArray
-            ? (result || []).concat(value)
-            : Object.assign(result || {}, { [camelize(key)]: value });
-        } else {
-          return result;
-        }
-      }, undefined);
-  } else {
-    return subject;
-  }
-}
+const { isArray } = Array;
+const { entries, assign } = Object;
 
 // Edge case camelizations
 const CAMELIZE_MAP = {
@@ -28,8 +8,30 @@ const CAMELIZE_MAP = {
 };
 
 // Converts a kebab-cased string to camelCase.
-function camelize(s) {
-  return s.replace(/-([^-]+)/g, (_, w) => (
+function camelize(str) {
+  return str.replace(/-([^-]+)/g, (_, w) => (
     CAMELIZE_MAP[w] || (w[0].toUpperCase() + w.slice(1))
   ));
+}
+
+// Merges source values into the target object unless empty. When `options.replaceArrays` is truthy,
+// target arrays are replaced by their source arrays rather than concatenated together.
+export function merge(target, source, options) {
+  let isSourceArray = isArray(source);
+  if (options?.replaceArrays && isSourceArray) return source;
+  if (typeof source !== 'object') return source != null ? source : target;
+
+  return entries(source).reduce((result, [key, value]) => {
+    value = merge(result?.[key], value, options);
+
+    return value == null ? result
+      : isSourceArray ? (result || []).concat(value)
+        : assign(result || {}, { [camelize(key)]: value });
+  }, target);
+}
+
+// Recursively reduces config objects and arrays to remove undefined and empty values and rename
+// kebab-case properties to camelCase. Optionally allows deep merging of override values
+export default function normalize(object, overrides) {
+  return merge(merge(undefined, object), overrides);
 }
