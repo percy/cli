6da52b9cc24a6b3f8272223a4c8176ba8bd6d94a
Wil Wilsman <wil@wilwilsman.com>
2020-12-21 16:47:20 -0600

    ✨ Enhance browser install script
    Decouple from puppeteer and allow optional arguments to control which browser binary to download. By
    default, it will download Chromium 78 which aligns with Percy's own Chromium version.

--- packages/core/src/utils/install-browser.js
+++ packages/core/src/utils/install-browser.js
@@ -1,56 +1,84 @@
-import fs from 'fs';
-import puppeteer from 'puppeteer-core';
+import os from 'os';
+import path from 'path';
+import https from 'https';
+import {
+  promises as fs,
+  existsSync,
+  createWriteStream
+} from 'fs';
 import log from '@percy/logger';
 import readableBytes from './bytes';
 
-// Get the default revisions from within puppeteer
-import { PUPPETEER_REVISIONS } from 'puppeteer-core/lib/cjs/puppeteer/revisions';
+export default async function install({
+  // default discovery browser is chromium
+  browser = 'Chromium',
+  // default chromium version is 78.0.3904.87
+  revision = '693954',
+  // default download directory is in @percy/core root
+  directory = path.resolve(__dirname, '../../.local-chromium'),
+  // default download url is dependent on platform and revision
+  url = `https://storage.googleapis.com/chromium-browser-snapshots/${{
+    linux: `Linux_x64/${revision}/chrome-linux.zip`,
+    darwin: `Mac/${revision}/chrome-mac.zip`,
+    win32: os.arch() === 'x64'
+      ? `Win_x64/${revision}/chrome-win64.zip`
+      : `Win/${revision}/chrome-win32.zip`
+  }[os.platform()]}`,
+  // default extraction method is to unzip
+  extract = (i, o) => require('extract-zip')(i, { dir: o }),
+  // default exectuable location within the extracted archive
+  executable = {
+    linux: 'chrome',
+    win32: 'chrome.exe',
+    darwin: path.join('Chromium.app', 'Contents', 'MacOS', 'Chromium')
+  }[os.platform()]
+} = {}) {
+  let outdir = path.join(directory, revision);
+  let dlpath = path.join(outdir, url.split('/').pop());
+  let exec = path.join(outdir, path.parse(dlpath).name, executable);
 
-// If the default Chromium revision is not yet downloaded, download it. Lazily
-// requires the progress package to print a progress bar during the download.
-export default async function maybeInstallBrowser(
-  path = process.env.PUPPETEER_EXECUTABLE_PATH
-) {
-  let revision = process.env.PUPPETEER_CHROMIUM_REVISION ||
-    PUPPETEER_REVISIONS.chromium;
-  let local = false;
-
-  if (path) {
-    if (!fs.existsSync(path)) {
-      log.error(`Puppeteer executable path not found: ${path}`);
-    } else {
-      return path;
-    }
-  }
-
-  let fetcher = puppeteer.createBrowserFetcher();
-  ({ executablePath: path, local, revision } = fetcher.revisionInfo(revision));
-
-  if (!local) {
-    let ProgressBar = require('progress');
-    let progress, last;
-
-    // we always want to log this
+  if (!existsSync(exec)) {
+    // always log this for progress bar context
     let loglevel = log.loglevel();
     log.loglevel('info');
-    log.info('Chromium not found, downloading...');
 
-    await fetcher.download(revision, (downloaded, total) => {
-      progress = progress || new ProgressBar(
-        log.formatter(`Chromium r${revision} - ${readableBytes(total)} [:bar] :percent :etas`),
-        { incomplete: ' ', width: 21, total, stream: process.stdout }
-      );
+    log.info(`${browser} not found, downloading...`);
 
-      progress.tick(downloaded - last);
-      last = downloaded;
+    // ensure the out directory exists
+    await fs.mkdir(outdir, { recursive: true });
+
+    // download the file at the given URL and log progress
+    await new Promise((resolve, reject) => {
+      https.get(url, response => {
+        let file = createWriteStream(dlpath);
+        let size = parseInt(response.headers['content-length'], 10);
+        let msg = `${revision} - ${readableBytes(size)} [:bar] :percent :etas`;
+        let progress = new (require('progress'))(log.formatter(msg), {
+          stream: process.stdout,
+          incomplete: ' ',
+          total: size,
+          width: 20
+        });
+
+        response.on('data', chunk => {
+          file.write(chunk);
+          progress.tick(chunk.length);
+        }).on('end', () => {
+          file.end();
+          resolve();
+        });
+      }).on('error', reject);
     });
 
-    process.stdout.write('\n');
-    log.info('Successfully downloaded Chromium');
-    log.loglevel(loglevel);
+    // extract the downloaded file and cleanup
+    await extract(dlpath, outdir);
+    await fs.unlink(dlpath);
 
-    ({ executablePath: path } = fetcher.revisionInfo(revision));
+    // log success and restore previous loglevel
+    log.info(`Successfully downloaded ${browser}`);
+    log.loglevel(loglevel);
   }
 
-  return path;
+  // return the path to the executable
+  return exec;
 }
