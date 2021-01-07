59f0e9700464143da8f8302bc1759a93e89e3a5d
Wil Wilsman <wil@percy.io>
2021-01-07 13:52:08 -0600

    ✨ Export CLI commands (#130)
    Allows for invoking commands via Node scripts without needing to invoke a subprocess.

--- packages/cli-config/src/index.js
+++ packages/cli-config/src/index.js
@@ -1,1 +1,5 @@
-export default {};
+const { Create } = require('./commands/config/create');
+const { Migrate } = require('./commands/build/migrate');
+const { Validate } = require('./commands/build/validate');
+
+module.exports = { Create, Migrate, Validate };
