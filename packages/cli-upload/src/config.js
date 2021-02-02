c30dbdbee197e8152b587d2b8aaac146fe6bef2d
Wil Wilsman <wil@wilwilsman.com>
2021-02-01 18:23:51 -0600

    ✨ Handle own upload config migration logic

--- packages/cli-upload/src/config.js
+++ packages/cli-upload/src/config.js
@@ -20,3 +20,14 @@ export const schema = {
     }
   }
 };
+
+export function migration(input, set) {
+  /* eslint-disable curly */
+  if (input.version < 2) {
+    // image-snapshots and options were renamed
+    if (input.imageSnapshots?.files != null)
+      set('upload.files', input.imageSnapshots.files);
+    if (input.imageSnapshots?.ignore != null)
+      set('upload.ignore', input.imageSnapshots.ignore);
+  }
+}
