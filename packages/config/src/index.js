ef2d247a441032ea2a9e669e3e42ecad9ed0528f
Wil Wilsman <wil@wilwilsman.com>
2021-02-03 14:05:42 -0600

    ✨ Export config normalize helper

--- packages/config/src/index.js
+++ packages/config/src/index.js
@@ -2,6 +2,7 @@ import load, { cache, explorer } from './load';
 import validate, { addSchema, resetSchema } from './validate';
 import migrate, { addMigration, clearMigrations } from './migrate';
 import getDefaults from './defaults';
+import normalize from './normalize';
 import stringify from './stringify';
 
 // Export a single object that can be imported as PercyConfig
@@ -16,5 +17,6 @@ export default {
   addMigration,
   clearMigrations,
   getDefaults,
+  normalize,
   stringify
 };
