07717434ff09f8c08318f0354261e0e387a7824d
Wil Wilsman <wil@wilwilsman.com>
2021-02-03 14:06:05 -0600

    ✨ Normalize snapshot API options

--- packages/core/src/server.js
+++ packages/core/src/server.js
@@ -1,5 +1,6 @@
 import fs from 'fs';
 import http from 'http';
+import PercyConfig from '@percy/config';
 import logger from '@percy/logger';
 import pkg from '../package.json';
 
@@ -117,8 +118,10 @@ export default function createPercyServer(percy) {
       }),
 
     // forward snapshot requests
-    '/percy/snapshot': ({ body }) => percy.snapshot(body)
-      .then(() => [200, 'application/json', { success: true }]),
+    '/percy/snapshot': ({ body }) => (
+      percy.snapshot(PercyConfig.normalize(body))
+        .then(() => [200, 'application/json', { success: true }])
+    ),
 
     // stops the instance async (connections will be closed)
     '/percy/stop': () => percy.stop() && (
