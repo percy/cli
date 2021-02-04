29a39ff1c85f06685684e5fd8117c46c52118c1c
Wil Wilsman <wil@wilwilsman.com>
2021-02-03 18:48:02 -0600

    🐛 Work around incorrect globalThis

--- packages/dom/src/index.js
+++ packages/dom/src/index.js
@@ -1,1 +1,11 @@
-export { default, default as serialize } from './serialize-dom';
+import serialize from './serialize-dom';
+
+/* istanbul ignore next */
+// works around instances where the context has an incorrect global scope
+// https://github.com/mozilla/geckodriver/issues/1798
+if (globalThis !== window) {
+  window.PercyDOM = { serialize };
+}
+
+export { serialize };
+export default serialize;
