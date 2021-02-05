62440066f91cfe859f3244289c4c4eabff83444f
Wil Wilsman <wil@percy.io>
2021-02-05 10:25:06 -0600

    ✨ Prefer kebab-case for yaml config files (#167)
    * ✨ Allow normalize to accept options parameter as second arg
    
    Overrides can be nested within options since it is only used in one place
    
    * ✨ Allow normalizing options as kebab-cased
    
    * ✨ Prefer kebab-case for yaml migrations

--- packages/config/src/normalize.js
+++ packages/config/src/normalize.js
@@ -2,18 +2,31 @@ const { isArray } = Array;
 const { entries, assign } = Object;
 
 // Edge case camelizations
-const CAMELIZE_MAP = {
-  css: 'CSS',
-  javascript: 'JavaScript'
-};
+const CAMELCASE_MAP = new Map([
+  ['css', 'CSS'],
+  ['javascript', 'JavaScript']
+]);
 
 // Converts kebab-cased and snake_cased strings to camelCase.
 const KEBAB_SNAKE_REG = /[-_]([^-_]+)/g;
 
-function camelize(str) {
+function camelcase(str) {
   return str.replace(KEBAB_SNAKE_REG, (match, word) => (
-    CAMELIZE_MAP[word] || (word[0].toUpperCase() + word.slice(1))
+    CAMELCASE_MAP.get(word) || (word[0].toUpperCase() + word.slice(1))
   ));
+}
+
+// Coverts camelCased and snake_cased strings to kebab-case.
+const CAMEL_SNAKE_REG = /([a-z])([A-Z]+)|_([^_]+)/g;
+
+function kebabcase(str) {
+  return Array.from(CAMELCASE_MAP)
+    .reduce((str, [word, camel]) => (
+      str.replace(camel, `-${word}`)
+    ), str)
+    .replace(CAMEL_SNAKE_REG, (match, p, n, w) => (
+      `${p || ''}-${(n || w).toLowerCase()}`
+    ));
 }
 
 // Merges source values into the target object unless empty. When `options.replaceArrays` is truthy,
@@ -22,18 +35,23 @@ export function merge(target, source, options) {
   let isSourceArray = isArray(source);
   if (options?.replaceArrays && isSourceArray) return source;
   if (typeof source !== 'object') return source != null ? source : target;
+  let convertcase = options?.kebab ? kebabcase : camelcase;
 
   return entries(source).reduce((result, [key, value]) => {
     value = merge(result?.[key], value, options);
 
     return value == null ? result
       : isSourceArray ? (result || []).concat(value)
-        : assign(result || {}, { [camelize(key)]: value });
+        : assign(result || {}, { [convertcase(key)]: value });
   }, target);
 }
 
 // Recursively reduces config objects and arrays to remove undefined and empty values and rename
-// kebab-case properties to camelCase. Optionally allows deep merging of override values
-export default function normalize(object, overrides) {
-  return merge(merge(undefined, object), overrides);
+// kebab-case properties to camelCase. Optionally allows deep merging of a second overrides
+// argument, and converting keys to kebab-case with a third options.kebab argument.
+export default function normalize(object, options) {
+  object = merge(undefined, object, options);
+  return options?.overrides
+    ? merge(object, options.overrides, options)
+    : object;
 }
