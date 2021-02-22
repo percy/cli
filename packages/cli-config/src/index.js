b335190cfc99ed345d716569b8697703b6da8e56
Wil Wilsman <wil@wilwilsman.com>
2021-02-22 14:02:03 -0600

    🐛 Fix cli-config exports

--- packages/cli-config/src/index.js
+++ packages/cli-config/src/index.js
@@ -1,5 +1,5 @@
 const { Create } = require('./commands/config/create');
-const { Migrate } = require('./commands/build/migrate');
-const { Validate } = require('./commands/build/validate');
+const { Migrate } = require('./commands/config/migrate');
+const { Validate } = require('./commands/config/validate');
 
 module.exports = { Create, Migrate, Validate };
