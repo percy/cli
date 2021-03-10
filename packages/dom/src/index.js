c4f21807c3458748bb0cfa9f4de89f7e2bddf05f
Robert DeLuca <robertdeluca19@gmail.com>
2021-03-10 17:02:51 -0600

    :bug: Catch any errors that arise from `globalThis` in DOM libaries (#232)
    Nightmare/electron envs don't have `globalThis` defined, which errors and breaks
    snapshots. This wraps a try/catch around it to stop that from happening

--- packages/dom/src/index.js
+++ packages/dom/src/index.js
@@ -3,8 +3,12 @@ import serialize from './serialize-dom';
 /* istanbul ignore next */
 // works around instances where the context has an incorrect global scope
 // https://github.com/mozilla/geckodriver/issues/1798
-if (globalThis !== window) {
-  window.PercyDOM = { serialize };
+try {
+  if (globalThis !== window) {
+    window.PercyDOM = { serialize };
+  }
+} catch (error) {
+  // `globalThis` is probably not defined
 }
 
 export { serialize };
