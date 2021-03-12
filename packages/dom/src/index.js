065f007da23cb340e2f88c1621e825136b633ed9
Wil Wilsman <wil@wilwilsman.com>
2021-03-12 14:16:15 -0600

    🐛 Set PercyDOM global to module exports

--- packages/dom/src/index.js
+++ packages/dom/src/index.js
@@ -3,7 +3,7 @@ import serialize from './serialize-dom';
 /* istanbul ignore next */
 // works around instances where the context has an incorrect scope
 if (typeof window !== 'undefined') {
-  window.PercyDOM = { serialize };
+  window.PercyDOM = exports;
 }
 
 export { serialize };
