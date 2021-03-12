38e75c76861eddd3e4bfdb272568187aef474389
Wil Wilsman <wil@wilwilsman.com>
2021-03-12 12:14:23 -0600

    🐛 Force @percy/dom globalization
    Sometimes modules or exports are polyfilled causing the UMD bundle to not attach the DOM script to
    the global scope. Since this scope issue seems to keep coming up, let's always set the serialize
    function globally when the window object is defined.

--- packages/dom/src/index.js
+++ packages/dom/src/index.js
@@ -1,14 +1,9 @@
 import serialize from './serialize-dom';
 
 /* istanbul ignore next */
-// works around instances where the context has an incorrect global scope
-// https://github.com/mozilla/geckodriver/issues/1798
-try {
-  if (globalThis !== window) {
-    window.PercyDOM = { serialize };
-  }
-} catch (error) {
-  // `globalThis` is probably not defined
+// works around instances where the context has an incorrect scope
+if (typeof window !== 'undefined') {
+  window.PercyDOM = { serialize };
 }
 
 export { serialize };
