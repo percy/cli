2071a1f9c0a4f85e7164ef69082e375b04aa9676
Wil Wilsman <wil@wilwilsman.com>
2021-03-19 00:35:40 -0500

    ♻ Split up and refactor @percy/sdk-utils

---
+++ packages/sdk-utils/src/request.js
@@ -0,0 +1,58 @@
+import percy from './percy-info';
+
+// Helper to send a request to the local CLI API
+export default async function request(path, options = {}) {
+  let response = await request.fetch(`${percy.address}${path}`, options);
+
+  // maybe parse response body as json
+  if (typeof response.body === 'string' &&
+      response.headers['content-type'] === 'application/json') {
+    try { response.body = JSON.parse(response.body); } catch (e) {}
+  }
+
+  // throw an error if status is not ok
+  if (!(response.status >= 200 && response.status < 300)) {
+    throw Object.assign(new Error(), {
+      message: response.body.error ||
+        `${response.status} ${response.statusText}`,
+      response
+    });
+  }
+
+  return response;
+}
+
+// environment specific implementation
+if (process.env.__PERCY_BROWSERIFIED__) {
+  // use window.fetch in browsers
+  request.fetch = async function fetch(url, options) {
+    let response = await window.fetch(url, options);
+
+    return {
+      status: response.status,
+      statusText: response.statusText,
+      headers: Object.fromEntries(response.headers.entries()),
+      body: await response.text()
+    };
+  };
+} else {
+  // use http.request in node
+  request.fetch = async function fetch(url, options) {
+    return new Promise((resolve, reject) => {
+      require('http').request(url, options)
+        .on('response', response => {
+          let body = '';
+
+          response.on('data', chunk => (body += chunk.toString()));
+          response.on('end', () => resolve({
+            status: response.statusCode,
+            statusText: response.statusMessage,
+            headers: response.headers,
+            body
+          }));
+        })
+        .on('error', reject)
+        .end(options.body);
+    });
+  };
+}
