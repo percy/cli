89d47ca0d2e6e3667552b5f496ded9a98c56d334
Wil Wilsman <wil@wilwilsman.com>
2021-04-13 12:48:49 -0500

    ♻ Move snapshot discovery options

--- packages/core/src/config.js
+++ packages/core/src/config.js
@@ -17,18 +17,6 @@ export const schema = {
         type: 'string',
         default: ''
       },
-      requestHeaders: {
-        type: 'object',
-        additionalProperties: { type: 'string' }
-      },
-      authorization: {
-        type: 'object',
-        additionalProperties: false,
-        properties: {
-          username: { type: 'string' },
-          password: { type: 'string' }
-        }
-      },
       enableJavaScript: {
         type: 'boolean'
       }
@@ -51,6 +39,18 @@ export const schema = {
         type: 'boolean',
         default: false
       },
+      requestHeaders: {
+        type: 'object',
+        additionalProperties: { type: 'string' }
+      },
+      authorization: {
+        type: 'object',
+        additionalProperties: false,
+        properties: {
+          username: { type: 'string' },
+          password: { type: 'string' }
+        }
+      },
       concurrency: {
         type: 'integer'
       },
@@ -69,7 +69,7 @@ export const schema = {
 };
 
 // Migration function
-export function migration(config, { map, del }) {
+export function migration(config, { map, del, log }) {
   /* eslint-disable curly */
   if (config.version < 2) {
     // discovery options have moved
@@ -79,5 +79,15 @@ export function migration(config, { map, del }) {
     map('agent.assetDiscovery.requestHeaders', 'discovery.requestHeaders');
     map('agent.assetDiscovery.pagePoolSizeMax', 'discovery.concurrency');
     del('agent');
+  } else {
+    // snapshot discovery options have moved
+    for (let k of ['authorization', 'requestHeaders']) {
+      if (config.snapshot?.[k]) {
+        log.deprecated(
+          `The config option \`snapshot.${k}\` will be removed in 1.0.0. ` +
+          `Use \`discovery.${k}\` instead.`);
+        map(`snapshot.${k}`, `discovery.${k}`);
+      }
+    }
   }
 }
