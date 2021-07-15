495050aa1744d24f1d240e946266672d634c0e77
Wil Wilsman <wil@wilwilsman.com>
2021-07-15 12:20:26 -0500

    🐛 Correct json schema ref key

--- packages/cli-snapshot/src/config.js
+++ packages/cli-snapshot/src/config.js
@@ -38,7 +38,7 @@ export const snapshotListSchema = {
     items: {
       oneOf: [
         { $ref: '/snapshot' },
-        { type: '/snapshot#/properties/url' }
+        { $ref: '/snapshot#/properties/url' }
       ]
     }
   }, {
