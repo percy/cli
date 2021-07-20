2561a803ef1985423d4bafcbe236c7d1f95bd1a7
Wil Wilsman <wil@wilwilsman.com>
2021-07-20 12:04:33 -0500

    🐛 Ensure core schemas are loaded before dependent snapshot schemas

--- packages/cli-snapshot/src/hooks/init.js
+++ packages/cli-snapshot/src/hooks/init.js
@@ -1,7 +1,9 @@
 import PercyConfig from '@percy/config';
+import * as CoreConfig from '@percy/core/dist/config';
 import * as SnapshotConfig from '../config';
 
 export default function() {
+  PercyConfig.addSchema(CoreConfig.schemas);
   PercyConfig.addSchema(SnapshotConfig.schemas);
   PercyConfig.addMigration(SnapshotConfig.migration);
 }
