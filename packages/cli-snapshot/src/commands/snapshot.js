hixio-mh/cli-6

💥✨ Add include/exclude cli-snapshot options & deprecate files/ignore
The `files` and `ignore` options were static snapshot only options, while the new `include` and
`exclude` options will work for both static snapshots and snapshot list files. These two new options
also accept string patterns, regular expressions, and functions, in addition to globs.

References to "pages" were replaced with references to "snapshots" instead. Since the merger of core
`#capture()` and `#snapshot()` methods, the distinction is moot. Other small naming changes were
made as well in reference to internal methods and utils.

The static snapshots method will now only pass strings to globby, while continuing to pass all
include/exclude options on to the static mapping util. Snapshot list files can now also accept
top-level include, exclude, and base-url options.

packages/cli-snapshot/src/commands/snapshot.js
@@ -6,9 +6,13 @@ import Percy from '@percy/core';
 import logger from '@percy/logger';
 import globby from 'globby';
 import YAML from 'yaml';
-import { configSchema } from '../config';
-import { serve, withDefaults, mapPages } from '../utils';
 import pkg from '../../package.json';
+import {
+  serve,
+  withDefaults,
+  snapshotMatches,
+  mapStaticSnapshots
+} from '../utils';
 
 export class Snapshot extends Command {
   static description = 'Snapshot a list of pages from a file or directory';
@@ -28,27 +32,34 @@ export class Snapshot extends Command {
       description: 'the base url pages are hosted at when snapshotting',
       char: 'b'
     }),
+    include: flags.string({
+      description: 'one or more globs matching snapshots paths to include',
+      multiple: true
+    }),
+    exclude: flags.string({
+      description: 'one or more globs matching snapshot paths to exclude',
+      multiple: true
+    }),
     'dry-run': flags.boolean({
-      description: 'prints a list of pages to snapshot without snapshotting',
+      description: 'prints a list of snapshots without processing them',
       char: 'd'
     }),
 
     // static only flags
-    files: flags.string({
-      description: 'one or more globs matching static file paths to snapshot',
-      default: configSchema.static.properties.files.default,
-      percyrc: 'static.files',
-      multiple: true
-    }),
-    ignore: flags.string({
-      description: 'one or more globs matching static file paths to ignore',
-      default: configSchema.static.properties.ignore.default,
-      percyrc: 'static.ignore',
-      multiple: true
-    }),
     'clean-urls': flags.boolean({
       description: 'rewrite static index and filepath URLs to be clean',
       percyrc: 'static.cleanUrls'
+    }),
+    // deprecated flags
+    files: flags.string({
+      deprecated: { map: 'include', until: '1.0.0' },
+      percyrc: 'static.include',
+      hidden: true
+    }),
+    ignore: flags.string({
+      deprecated: { map: 'exclude', until: '1.0.0' },
+      percyrc: 'static.exclude',
+      hidden: true
     })
   };
 
@@ -60,45 +71,54 @@ export class Snapshot extends Command {
   log = logger('cli:snapshot')
 
   async run() {
+    // skip snapshots
     if (!this.isPercyEnabled()) {
       return this.log.info('Percy is disabled. Skipping snapshots');
     }
 
+    // validate path existence
     let { pathname } = this.args;
+    if (!fs.existsSync(pathname)) this.error(`Not found: ${pathname}`);
+    let isStatic = fs.lstatSync(pathname).isDirectory();
 
-    if (!fs.existsSync(pathname)) {
-      this.error(`Not found: ${pathname}`);
-    }
-
+    // initialize percy
     this.percy = new Percy({
-      ...this.percyrc(),
+      ...this.percyrc({
+        static: isStatic ? {
+          include: this.flags.include,
+          exclude: this.flags.exclude
+        } : undefined
+      }),
+
       clientInfo: `${pkg.name}/${pkg.version}`,
       server: false
     });
 
-    let pages = fs.lstatSync(pathname).isDirectory()
-      ? await this.loadStaticPages(pathname)
-      : await this.loadPagesFile(pathname);
+    // gather snapshots
+    let snapshots = isStatic
+      ? await this.loadStaticSnapshots(pathname)
+      : await this.loadSnapshotsFile(pathname);
 
-    let l = pages.length;
+    let l = snapshots.length;
     if (!l) this.error('No snapshots found');
 
+    // start processing snapshots
     let dry = this.flags['dry-run'];
     if (!dry) await this.percy.start();
     else this.log.info(`Found ${l} snapshot${l === 1 ? '' : 's'}`);
 
-    for (let page of pages) {
+    for (let snap of snapshots) {
       if (dry) {
-        this.log.info(`Snapshot found: ${page.name}`);
-        this.log.debug(`-> url: ${page.url}`);
+        this.log.info(`Snapshot found: ${snap.name}`);
+        this.log.debug(`-> url: ${snap.url}`);
 
-        for (let s of (page.additionalSnapshots || [])) {
-          let name = s.name || `${s.prefix || ''}${page.name}${s.suffix || ''}`;
+        for (let s of (snap.additionalSnapshots || [])) {
+          let name = s.name || `${s.prefix || ''}${snap.name}${s.suffix || ''}`;
           this.log.info(`Snapshot found: ${name}`);
-          this.log.debug(`-> url: ${page.url}`);
+          this.log.debug(`-> url: ${snap.url}`);
         }
       } else {
-        this.percy.snapshot(page);
+        this.percy.snapshot(snap);
       }
     }
   }
@@ -110,56 +130,76 @@ export class Snapshot extends Command {
   }
 
   // Starts a static server and returns a list of pages to snapshot.
-  async loadStaticPages(pathname) {
+  async loadStaticSnapshots(pathname) {
     let config = this.percy.config.static;
     let baseUrl = this.flags['base-url'] || config.baseUrl;
     let dry = this.flags['dry-run'];
 
+    // validate any provided base-url
     if (baseUrl && !baseUrl.startsWith('/')) {
       this.error('The base-url must begin with a forward slash (/) ' + (
         'when snapshotting static directories'));
     }
 
+    // start the server
     this.server = await serve(pathname, { ...config, baseUrl, dry });
-
     let { host, rewrites } = this.server;
-    let ignore = [].concat(config.ignore || []);
-    let paths = await globby(config.files, { cwd: pathname, ignore });
-    return mapPages(paths, { ...config, host, rewrites });
+
+    // gather paths and map snapshots
+    let isStr = s => typeof s === 'string';
+    let strOr = (a, b) => a.length && a.every(isStr) ? a : b;
+    let files = strOr([].concat(config.include || []), '**/*.html');
+    let ignore = strOr([].concat(config.exclude || []), []);
+    let paths = await globby(files, { cwd: pathname, ignore });
+    return mapStaticSnapshots(paths, { ...config, host, rewrites });
   }
 
   // Loads pages to snapshot from a js, json, or yaml file.
-  async loadPagesFile(pathname) {
+  async loadSnapshotsFile(pathname) {
     let ext = path.extname(pathname);
-    let baseUrl = this.flags['base-url'];
-    let pages = [];
+    let config = {};
 
+    // load snapshots file
+    if (ext === '.js') {
+      config = require(path.resolve(pathname));
+      if (typeof config === 'function') config = await config();
+    } else if (ext === '.json') {
+      config = JSON.parse(fs.readFileSync(pathname, { encoding: 'utf-8' }));
+    } else if (ext.match(/\.ya?ml$/)) {
+      config = YAML.parse(fs.readFileSync(pathname, { encoding: 'utf-8' }));
+    } else {
+      this.error(`Unsupported filetype: ${pathname}`);
+    }
+
+    let {
+      // flags override config options
+      'base-url': baseUrl = config.baseUrl,
+      include = config.include,
+      exclude = config.exclude
+    } = this.flags;
+
+    // validate base-url before config options
     if (baseUrl && !baseUrl.startsWith('http')) {
       this.error('The base-url must include a protocol and hostname ' + (
         'when snapshotting a list of pages'));
     }
 
-    if (ext === '.js') {
-      pages = require(path.resolve(pathname));
-      if (typeof pages === 'function') pages = await pages();
-    } else if (ext === '.json') {
-      pages = JSON.parse(fs.readFileSync(pathname, { encoding: 'utf-8' }));
-    } else if (ext.match(/\.ya?ml$/)) {
-      pages = YAML.parse(fs.readFileSync(pathname, { encoding: 'utf-8' }));
-    } else {
-      this.error(`Unsupported filetype: ${pathname}`);
-    }
-
-    // validate page listings
-    let errors = PercyConfig.validate(pages, '/snapshot/list');
+    // validate snapshot config options
+    let errors = PercyConfig.validate(config, '/snapshot/list');
 
     if (errors) {
       this.log.warn('Invalid snapshot options:');
       for (let e of errors) this.log.warn(`- ${e.path}: ${e.message}`);
     }
 
-    // support snapshots option for yaml references and lists of urls
-    return (Array.isArray(pages) ? pages : pages.snapshots || [])
-      .map(page => withDefaults(page, { host: baseUrl }));
+    // support config that only contains a list of snapshots
+    return (Array.isArray(config) ? config : config.snapshots || [])
+      .reduce((snapshots, snap) => {
+        // reduce matching snapshots with default options
+        snap = withDefaults(snap, { host: baseUrl });
+
+        return snapshotMatches(snap, include, exclude)
+          ? snapshots.concat(snap) : snapshots;
+      }, []);
   }
 }

percy/cli

packages/cli-snapshot/src/commands/snapshot.js
@@ -6,9 +6,13 @@ import Percy from '@percy/core';
 import logger from '@percy/logger';
 import globby from 'globby';
 import YAML from 'yaml';
-import { configSchema } from '../config';
-import { serve, withDefaults, mapPages } from '../utils';
 import pkg from '../../package.json';
+import {
+  serve,
+  withDefaults,
+  snapshotMatches,
+  mapStaticSnapshots
+} from '../utils';
 
 export class Snapshot extends Command {
   static description = 'Snapshot a list of pages from a file or directory';
@@ -28,27 +32,34 @@ export class Snapshot extends Command {
       description: 'the base url pages are hosted at when snapshotting',
       char: 'b'
     }),
+    include: flags.string({
+      description: 'one or more globs matching snapshots paths to include',
+      multiple: true
+    }),
+    exclude: flags.string({
+      description: 'one or more globs matching snapshot paths to exclude',
+      multiple: true
+    }),
     'dry-run': flags.boolean({
-      description: 'prints a list of pages to snapshot without snapshotting',
+      description: 'prints a list of snapshots without processing them',
       char: 'd'
     }),
 
     // static only flags
-    files: flags.string({
-      description: 'one or more globs matching static file paths to snapshot',
-      default: configSchema.static.properties.files.default,
-      percyrc: 'static.files',
-      multiple: true
-    }),
-    ignore: flags.string({
-      description: 'one or more globs matching static file paths to ignore',
-      default: configSchema.static.properties.ignore.default,
-      percyrc: 'static.ignore',
-      multiple: true
-    }),
     'clean-urls': flags.boolean({
       description: 'rewrite static index and filepath URLs to be clean',
       percyrc: 'static.cleanUrls'
+    }),
+    // deprecated flags
+    files: flags.string({
+      deprecated: { map: 'include', until: '1.0.0' },
+      percyrc: 'static.include',
+      hidden: true
+    }),
+    ignore: flags.string({
+      deprecated: { map: 'exclude', until: '1.0.0' },
+      percyrc: 'static.exclude',
+      hidden: true
     })
   };
 
@@ -60,45 +71,54 @@ export class Snapshot extends Command {
   log = logger('cli:snapshot')
 
   async run() {
+    // skip snapshots
     if (!this.isPercyEnabled()) {
       return this.log.info('Percy is disabled. Skipping snapshots');
     }
 
+    // validate path existence
     let { pathname } = this.args;
+    if (!fs.existsSync(pathname)) this.error(`Not found: ${pathname}`);
+    let isStatic = fs.lstatSync(pathname).isDirectory();
 
-    if (!fs.existsSync(pathname)) {
-      this.error(`Not found: ${pathname}`);
-    }
-
+    // initialize percy
     this.percy = new Percy({
-      ...this.percyrc(),
+      ...this.percyrc({
+        static: isStatic ? {
+          include: this.flags.include,
+          exclude: this.flags.exclude
+        } : undefined
+      }),
+
       clientInfo: `${pkg.name}/${pkg.version}`,
       server: false
     });
 
-    let pages = fs.lstatSync(pathname).isDirectory()
-      ? await this.loadStaticPages(pathname)
-      : await this.loadPagesFile(pathname);
+    // gather snapshots
+    let snapshots = isStatic
+      ? await this.loadStaticSnapshots(pathname)
+      : await this.loadSnapshotsFile(pathname);
 
-    let l = pages.length;
+    let l = snapshots.length;
     if (!l) this.error('No snapshots found');
 
+    // start processing snapshots
     let dry = this.flags['dry-run'];
     if (!dry) await this.percy.start();
     else this.log.info(`Found ${l} snapshot${l === 1 ? '' : 's'}`);
 
-    for (let page of pages) {
+    for (let snap of snapshots) {
       if (dry) {
-        this.log.info(`Snapshot found: ${page.name}`);
-        this.log.debug(`-> url: ${page.url}`);
+        this.log.info(`Snapshot found: ${snap.name}`);
+        this.log.debug(`-> url: ${snap.url}`);
 
-        for (let s of (page.additionalSnapshots || [])) {
-          let name = s.name || `${s.prefix || ''}${page.name}${s.suffix || ''}`;
+        for (let s of (snap.additionalSnapshots || [])) {
+          let name = s.name || `${s.prefix || ''}${snap.name}${s.suffix || ''}`;
           this.log.info(`Snapshot found: ${name}`);
-          this.log.debug(`-> url: ${page.url}`);
+          this.log.debug(`-> url: ${snap.url}`);
         }
       } else {
-        this.percy.snapshot(page);
+        this.percy.snapshot(snap);
       }
     }
   }
@@ -109,57 +129,77 @@ export class Snapshot extends Command {
     await this.server?.close();
   }
 
-  // Starts a static server and returns a list of pages to snapshot.
-  async loadStaticPages(pathname) {
+  // Serves a static directory and returns a list of snapshots.
+  async loadStaticSnapshots(dir) {
     let config = this.percy.config.static;
     let baseUrl = this.flags['base-url'] || config.baseUrl;
     let dry = this.flags['dry-run'];
 
+    // validate any provided base-url
     if (baseUrl && !baseUrl.startsWith('/')) {
       this.error('The base-url must begin with a forward slash (/) ' + (
         'when snapshotting static directories'));
     }
 
-    this.server = await serve(pathname, { ...config, baseUrl, dry });
-
+    // start the server
+    this.server = await serve(dir, { ...config, baseUrl, dry });
     let { host, rewrites } = this.server;
-    let ignore = [].concat(config.ignore || []);
-    let paths = await globby(config.files, { cwd: pathname, ignore });
-    return mapPages(paths, { ...config, host, rewrites });
+
+    // gather paths and map snapshots
+    let isStr = s => typeof s === 'string';
+    let strOr = (a, b) => a.length && a.every(isStr) ? a : b;
+    let files = strOr([].concat(config.include || []), '**/*.html');
+    let ignore = strOr([].concat(config.exclude || []), []);
+    let paths = await globby(files, { cwd: dir, ignore });
+    return mapStaticSnapshots(paths, { ...config, host, rewrites });
   }
 
-  // Loads pages to snapshot from a js, json, or yaml file.
-  async loadPagesFile(pathname) {
-    let ext = path.extname(pathname);
-    let baseUrl = this.flags['base-url'];
-    let pages = [];
+  // Loads snapshots from a js, json, or yaml file.
+  async loadSnapshotsFile(file) {
+    let ext = path.extname(file);
+    let config = {};
 
+    // load snapshots file
+    if (ext === '.js') {
+      config = require(path.resolve(file));
+      if (typeof config === 'function') config = await config();
+    } else if (ext === '.json') {
+      config = JSON.parse(fs.readFileSync(file, { encoding: 'utf-8' }));
+    } else if (ext.match(/\.ya?ml$/)) {
+      config = YAML.parse(fs.readFileSync(file, { encoding: 'utf-8' }));
+    } else {
+      this.error(`Unsupported filetype: ${file}`);
+    }
+
+    let {
+      // flags override config options
+      'base-url': baseUrl = config.baseUrl,
+      include = config.include,
+      exclude = config.exclude
+    } = this.flags;
+
+    // validate base-url before config options
     if (baseUrl && !baseUrl.startsWith('http')) {
       this.error('The base-url must include a protocol and hostname ' + (
         'when snapshotting a list of pages'));
     }
 
-    if (ext === '.js') {
-      pages = require(path.resolve(pathname));
-      if (typeof pages === 'function') pages = await pages();
-    } else if (ext === '.json') {
-      pages = JSON.parse(fs.readFileSync(pathname, { encoding: 'utf-8' }));
-    } else if (ext.match(/\.ya?ml$/)) {
-      pages = YAML.parse(fs.readFileSync(pathname, { encoding: 'utf-8' }));
-    } else {
-      this.error(`Unsupported filetype: ${pathname}`);
-    }
-
-    // validate page listings
-    let errors = PercyConfig.validate(pages, '/snapshot/list');
+    // validate snapshot config options
+    let errors = PercyConfig.validate(config, '/snapshot/list');
 
     if (errors) {
       this.log.warn('Invalid snapshot options:');
       for (let e of errors) this.log.warn(`- ${e.path}: ${e.message}`);
     }
 
-    // support snapshots option for yaml references and lists of urls
-    return (Array.isArray(pages) ? pages : pages.snapshots || [])
-      .map(page => withDefaults(page, { host: baseUrl }));
+    // support config that only contains a list of snapshots
+    return (Array.isArray(config) ? config : config.snapshots || [])
+      .reduce((snapshots, snap) => {
+        // reduce matching snapshots with default options
+        snap = withDefaults(snap, { host: baseUrl });
+
+        return snapshotMatches(snap, include, exclude)
+          ? snapshots.concat(snap) : snapshots;
+      }, []);
   }
 }