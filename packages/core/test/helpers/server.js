936773abe57b6f5502a76e0f1a0db226748d5974
Wil Wilsman <wil@wilwilsman.com>
2022-02-15 17:18:25 -0600

    ✅ Adjust test helpers for better compat

--- packages/core/test/helpers/server.js
+++ packages/core/test/helpers/server.js
@@ -4,16 +4,26 @@ const { default: Server } = require('@percy/core/dist/server');
 function createTestServer({ default: defaultReply, ...replies }, port = 8000) {
   let server = new Server();
 
-  // alternate route registration
-  server.reply = (p, cb) => (replies[p] = cb);
+  // alternate route handling
+  let handleReply = reply => async (req, res) => {
+    let [status, headers, body] = typeof reply === 'function' ? await reply(req) : reply;
+    if (!Buffer.isBuffer(body) && typeof body !== 'string') body = JSON.stringify(body);
+    return res.send(status, headers, body);
+  };
+
+  // map replies to alternate route handlers
+  server.reply = (p, reply) => (replies[p] = handleReply(reply));
+  for (let [p, reply] of Object.entries(replies)) server.reply(p, reply);
+  if (defaultReply) defaultReply = handleReply(defaultReply);
 
   // track requests and route replies
   server.requests = [];
   server.route(async (req, res, next) => {
-    let { url: { pathname } } = req;
-    let reply = replies[pathname] ?? defaultReply;
+    let pathname = req.url.pathname;
+    if (req.url.search) pathname += req.url.search;
     server.requests.push(req.body ? [pathname, req.body] : [pathname]);
-    return reply ? res.send(...await reply(req)) : next();
+    let reply = replies[req.url.pathname] || defaultReply;
+    return reply ? await reply(req, res) : next();
   });
 
   // automatically listen
