c78d820d1810423a2e091c90a1caf611963a5225
Wil Wilsman <wil@wilwilsman.com>
2022-05-12 16:15:39 -0500

    🚨 Remove empty comment

--- packages/core/src/browser.js
+++ packages/core/src/browser.js
@@ -218,7 +218,6 @@ export class Browser extends EventEmitter {
     }
   }
 
-  //
   async spawn(timeout = 30000) {
     // spawn the browser process detached in its own group and session
     this.process = spawn(this.executable, this.args, {
