0baa93616d6183a8ca79e5585b7cece079689897
Wil Wilsman <wil@wilwilsman.com>
2022-09-01 14:43:35 -0500

    🐛 Serialize all CSSOM rules
    Previously, we excluded serializing CSSOM rules that were already seemingly included in the
    document. However, JavaScript can still manipulate this CSSOM without updating the document, which
    prevents some CSSOM changes from being properly serialized. By removing this exclusion, existing
    CSSOM style content will also be serialized, which will include any manipulated CSSOM.

--- packages/dom/src/serialize-cssom.js
+++ packages/dom/src/serialize-cssom.js
@@ -1,8 +1,7 @@
 // Returns true if a stylesheet is a CSSOM-based stylesheet.
 function isCSSOM(styleSheet) {
-  // no href, has a rulesheet, and isn't already in the DOM
-  return !styleSheet.href && styleSheet.cssRules &&
-    !styleSheet.ownerNode?.innerText?.trim().length;
+  // no href, has a rulesheet, and has an owner node
+  return !styleSheet.href && styleSheet.cssRules && styleSheet.ownerNode;
 }
 
 // Outputs in-memory CSSOM into their respective DOM nodes.
@@ -14,11 +13,13 @@ export function serializeCSSOM(dom, clone) {
       let cloneOwnerNode = clone.querySelector(`[data-percy-element-id="${styleId}"]`);
 
       style.type = 'text/css';
+      style.setAttribute('data-percy-element-id', styleId);
       style.setAttribute('data-percy-cssom-serialized', 'true');
       style.innerHTML = Array.from(styleSheet.cssRules)
-        .reduce((prev, cssRule) => prev + cssRule.cssText, '');
+        .map(cssRule => cssRule.cssText).join('\n');
 
       cloneOwnerNode.parentNode.insertBefore(style, cloneOwnerNode.nextSibling);
+      cloneOwnerNode.remove();
     }
   }
 }
