15780cab553a5f2de267ffb70df55b3eda0a0375
Andrew Moore <15974756+andrewdmoore408@users.noreply.github.com>
2022-11-23 16:33:07 -0500

    Add an additional test for builds with no PR

--- packages/env/test/harness.test.js
+++ packages/env/test/harness.test.js
@@ -8,14 +8,30 @@ describe('Harness', () => {
       PERCY_PARALLEL_TOTAL: '-1',
       DRONE_COMMIT_BRANCH: 'harness-branch',
       DRONE_COMMIT_SHA: 'harness-commit-sha',
-      DRONE_BUILD_EVENT: 'pull_request',
-      DRONE_COMMIT_LINK: 'https://github.com/owner/repo-name/pull/718',
       HARNESS_BUILD_ID: '49',
       HARNESS_PROJECT_ID: 'harness-project-1'
     });
   });
 
-  it('has the correct properties', () => {
+  it('has the correct properties with no pull request', () => {
+    expect(env).toHaveProperty('ci', 'harness');
+    expect(env).toHaveProperty('commit', 'harness-commit-sha');
+    expect(env).toHaveProperty('branch', 'harness-branch');
+    expect(env).toHaveProperty('target.commit', null);
+    expect(env).toHaveProperty('target.branch', null);
+    expect(env).toHaveProperty('pullRequest', '718');
+    expect(env).toHaveProperty('parallel.nonce', '49');
+    expect(env).toHaveProperty('parallel.total', -1);
+  });
+
+  it('has the correct properties with a pull request', () => {
+    env = new PercyEnv({
+      ...env.vars,
+      DRONE_SOURCE_BRANCH: 'harness-branch',
+      DRONE_BUILD_EVENT: 'pull_request',
+      DRONE_COMMIT_LINK: 'https://github.com/owner/repo-name/pull/718',
+    });
+
     expect(env).toHaveProperty('ci', 'harness');
     expect(env).toHaveProperty('commit', 'harness-commit-sha');
     expect(env).toHaveProperty('branch', 'harness-branch');
