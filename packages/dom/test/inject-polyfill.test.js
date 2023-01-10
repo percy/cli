44ae9a5a4b4982419db548563238bb410b57f03f
Jigar Wala <jigarwala007@gmail.com>
2023-01-10 18:37:04 +0530

    add tests for inject polyfill

---
+++ packages/dom/test/inject-polyfill.test.js
@@ -0,0 +1,26 @@
+import injectDeclarativeShadowDOMPolyfill from '../src/inject-polyfill';
+import { withExample } from './helpers';
+
+describe('injectDeclarativeShadowDOMPolyfill', () => {
+  let dom;
+
+  beforeEach(() => {
+    dom = withExample(`
+        <div data-percy-shadow-host>
+        <template shadowroot="open">
+                <div data-percy-shadow-host>
+                    <template shadowroot="open">
+                    </template>
+                </div>
+        </template>
+        </div>
+        `, { withShadow: false });
+
+    let ctx = { clone: dom };
+    injectDeclarativeShadowDOMPolyfill(ctx);
+  });
+
+  it('All template tags are converted to Shadow DOM', () => {
+    expect(Array.from(dom.documentElement.innerHTML.matchAll(/<\/template>/g)).length).toBe(0);
+  });
+});
