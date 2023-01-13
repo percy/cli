f847f49db846eb0b11ab4864d9951b636d995453
Samarjeet <samarjeet@browserstack.com>
2023-01-13 11:03:41 +0530

    disableShadowDom option

--- packages/dom/src/prepare-dom.js
+++ packages/dom/src/prepare-dom.js
@@ -3,7 +3,7 @@ function uid() {
   return `_${Math.random().toString(36).substr(2, 9)}`;
 }
 
-export function markElement(domElement) {
+export function markElement(domElement, disableShadowDom = false) {
   // Mark elements that are to be serialized later with a data attribute.
   if (['input', 'textarea', 'select', 'iframe', 'canvas', 'video', 'style'].includes(domElement.tagName?.toLowerCase())) {
     if (!domElement.getAttribute('data-percy-element-id')) {
@@ -12,7 +12,7 @@ export function markElement(domElement) {
   }
 
   // add special marker for shadow host
-  if (domElement.shadowRoot) {
+  if (!disableShadowDom && domElement.shadowRoot) {
     if (!domElement.getAttribute('data-percy-shadow-host')) {
       domElement.setAttribute('data-percy-shadow-host', '');
     }
