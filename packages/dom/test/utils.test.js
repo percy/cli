d5ba1938ea150ec4812a7418426449f684db8df9
Samarjeet <samarjeet@browserstack.com>
2023-03-06 14:08:58 +0530

    add test for nonce

---
+++ packages/dom/test/utils.test.js
@@ -0,0 +1,13 @@
+import { styleSheetFromNode } from '../src/utils';
+
+describe('utils', () => {
+  it('creates styleSheetFromNode properly', () => {
+    const node = document.createElement('style');
+    node.innerText = 'p { background-color: red }';
+    const cloneSpy = spyOn(node, 'cloneNode').and.callThrough();
+    const sheet = styleSheetFromNode(node);
+    expect(sheet.cssRules[0].cssText).toEqual('p { background-color: red; }');
+    // nonce needs to be copied
+    expect(cloneSpy).toHaveBeenCalled();
+  });
+});
