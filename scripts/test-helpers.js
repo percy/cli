f04e135a678bf66c80c39be2e35faa8e8b35c1d6
Samarjeet <samarjeet@browserstack.com>
2023-04-14 17:11:03 +0530

    reduce timeout

--- scripts/test-helpers.js
+++ scripts/test-helpers.js
@@ -4,7 +4,7 @@ const env = jasmine.getEnv();
 
 beforeAll(() => {
   // default timeout to 10s
-  jasmine.DEFAULT_TIMEOUT_INTERVAL = 30000;
+  jasmine.DEFAULT_TIMEOUT_INTERVAL = 20000;
 
   // allow re-spying
   env.allowRespy(true);
