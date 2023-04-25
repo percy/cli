c3a7063b68127cd7119bb42c141d1b21f1a2dbaf
nilshah98 <neelshah.1998@gmail.com>
2023-04-26 00:03:24 +0530

    [test] add commonMetaDataResolver.test.js

--- packages/cli-poa/src/metadata/commonMetaDataResolver.js
+++ packages/cli-poa/src/metadata/commonMetaDataResolver.js
@@ -2,7 +2,7 @@ import CommonDesktopMetaDataResolver from './commonDesktopMetaDataResolver.js';
 import CommonMobileMetaDataResolver from './commonMobileMetaDetaResolver.js';
 
 export default class CommonMetaDataResolver {
-  static async resolve(driver, capabilities, opts) {
+  static resolve(driver, capabilities, opts) {
     const platform = opts.platformName || opts.platform;
     if (['ios', 'android'].includes(platform.toLowerCase())) {
       return new CommonMobileMetaDataResolver(driver, capabilities);
---
+++ packages/cli-poa/test/metadata/commonMetaDataResolver.test.js
@@ -0,0 +1,31 @@
+import CommonMetaDataResolver from "../../src/metadata/commonMetaDataResolver.js";
+import Driver from "../../src/driver.js"
+import CommonDesktopMetaDataResolver from "../../src/metadata/commonDesktopMetaDataResolver.js";
+import CommonMobileMetaDataResolver from "../../src/metadata/commonMobileMetaDetaResolver.js";
+
+describe('CommonMetaDataResolver', () => {
+  describe('resolve', () => {
+    let driver;
+    let capabilities;
+    let metadata;
+    
+    beforeEach(() => {
+      driver = new Driver('123', 'http:executorUrl');
+      capabilities = {};
+    })
+    
+    it('resolves CommonMobileMetaData correctly', () => {
+      metadata = CommonMetaDataResolver.resolve(driver, capabilities, {platform: 'ios'});
+      expect(metadata).toBeInstanceOf(CommonMobileMetaDataResolver);
+      expect(metadata.driver).toEqual(driver);
+      expect(metadata.capabilities).toEqual({});
+    })
+
+    it('resolves CommonDesktopMetaData correctly', () => {
+      metadata = CommonMetaDataResolver.resolve(driver, capabilities, {platform: 'win'});
+      expect(metadata).toBeInstanceOf(CommonDesktopMetaDataResolver);
+      expect(metadata.driver).toEqual(driver);
+      expect(metadata.capabilities).toEqual({});
+    })
+  })
+})
