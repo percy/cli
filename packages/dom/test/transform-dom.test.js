7d8b52c419f850e3d42653254a45d870b19ab2e8
Ninad Sheth <ninad@browserstack.com>
2023-05-23 17:12:32 +0530

    Added shadow dom tests

--- packages/dom/test/transform-dom.test.js
+++ packages/dom/test/transform-dom.test.js
@@ -1,34 +1,39 @@
 import { dropLoadingAttribute } from '../src/transform-dom';
-import { withExample } from './helpers';
+import { withExample, platforms, platformDOM } from './helpers';
 
 // Note: applyElementTransformations is tested in serialize-dom tests
 
-describe('dropLoadingAttribute', () => {
-  let dom;
+describe('transformDOM', () => {
+  describe('dropLoadingAttribute', () => {
+    let dom;
 
-  beforeEach(() => {
-    dom = withExample(`
-        <img id="image" loading="lazy"/>
-        <video id="other_tag" loading="lazy"/>
-        <iframe id="frame1" loading="lazy"/>
-        `);
-  });
+    platforms.forEach((platform) => {
+      beforeEach(() => {
+        withExample(`
+          <img id="image" loading="lazy"/>
+          <video id="other_tag" loading="lazy"/>
+          <iframe id="frame1" loading="lazy"/>
+        `, { withShadow: platform === 'shadow' });
+        dom = platformDOM(platform);
+      });
 
-  it('drops loading from image tag', () => {
-    dropLoadingAttribute(dom.getElementById('image'));
+      it(`${platform} drops loading from image tag`, () => {
+        dropLoadingAttribute(dom.getElementById('image'));
 
-    expect(dom.getElementById('image').getAttribute('loading')).toBe(null);
-  });
+        expect(dom.getElementById('image').getAttribute('loading')).toBe(null);
+      });
 
-  it('drops loading from iframe tag', () => {
-    dropLoadingAttribute(dom.getElementById('frame1'));
+      it(`${platform} drops loading from iframe tag`, () => {
+        dropLoadingAttribute(dom.getElementById('frame1'));
 
-    expect(dom.getElementById('frame1').getAttribute('loading')).toBe(null);
-  });
+        expect(dom.getElementById('frame1').getAttribute('loading')).toBe(null);
+      });
 
-  it('does not drop loading from other tags', () => {
-    dropLoadingAttribute(dom.getElementById('other_tag'));
+      it(`${platform} does not drop loading from other tags`, () => {
+        dropLoadingAttribute(dom.getElementById('other_tag'));
 
-    expect(dom.getElementById('other_tag').getAttribute('loading')).toBe('lazy');
+        expect(dom.getElementById('other_tag').getAttribute('loading')).toBe('lazy');
+      });
+    });
   });
 });
