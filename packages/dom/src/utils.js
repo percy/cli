fd07203b7649b6300f54ebdbb93662d886707f78
“Shahid <shahid.k@browserstack.com>
2023-06-19 16:58:38 +0530

    added test cases and resolved lint issues

--- packages/dom/src/utils.js
+++ packages/dom/src/utils.js
@@ -8,7 +8,7 @@ export function resourceFromDataURL(uid, dataURL) {
   // build a URL for the serialized asset
   let [, ext] = mimetype.split('/');
   let path = `/__serialized__/${uid}.${ext}`;
-  let url = document.URL.includes("localhost") ? new URL(path,"http://render.percy.local").toString() : new URL(path, document.URL).toString();
+  let url = document.URL.includes('localhost') ? new URL(path, 'http://render.percy.local').toString() : new URL(path, document.URL).toString();
 
   // return the url, base64 content, and mimetype
   return { url, content, mimetype };
@@ -18,7 +18,7 @@ export function resourceFromText(uid, mimetype, data) {
   // build a URL for the serialized asset
   let [, ext] = mimetype.split('/');
   let path = `/__serialized__/${uid}.${ext}`;
-  let url = document.URL.includes("localhost") ? new URL(path,"http://render.percy.local").toString() : new URL(path, document.URL).toString();
+  let url = document.URL.includes('localhost') ? new URL(path, 'http://render.percy.local').toString() : new URL(path, document.URL).toString();
   // return the url, text content, and mimetype
   return { url, content: data, mimetype };
 }
--- packages/dom/test/utils.test.js
+++ packages/dom/test/utils.test.js
@@ -1,5 +1,4 @@
-import { styleSheetFromNode } from '../src/utils';
-
+import { styleSheetFromNode, resourceFromDataURL, resourceFromText } from '../src/utils';
 describe('utils', () => {
   describe('styleSheetFromNode', () => {
     it('creates stylesheet properly', () => {
@@ -12,4 +11,68 @@ describe('utils', () => {
       expect(cloneSpy).toHaveBeenCalled();
     });
   });
+
+  describe('resourceFromDataURL', () => {
+
+    const uid = (Math.random() + 1).toString(36).substring(10);
+    const dataURL = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAHgAAAB4CAYAAAA5ZDbSAAAAAXNSR0IArs4c6QAACbVJREFUeF7tXAWoFVEQnW+'
+
+    it('If URL is localhost, replace it to render.percy.local', () => {
+
+      Object.defineProperty(window.document, 'URL', {
+        writable: true,
+        value: 'http://localhost',
+      });
+      const result = resourceFromDataURL(uid, dataURL);
+      expect(result).toEqual({
+        url: `http://render.percy.local/__serialized__/${uid}.png`,
+        content: 'iVBORw0KGgoAAAANSUhEUgAAAHgAAAB4CAYAAAA5ZDbSAAAAAXNSR0IArs4c6QAACbVJREFUeF7tXAWoFVEQnW+',
+        mimetype: 'image/png',  
+      });
+    });
+
+    it('If URL is not localhost, return as is', () => {
+      Object.defineProperty(window.document, 'URL', {
+        writable: true,
+        value: 'http://example.com',
+      });
+      const result = resourceFromDataURL(uid, dataURL);
+      expect(result).toEqual({
+        url: `http://example.com/__serialized__/${uid}.png`,
+        content: 'iVBORw0KGgoAAAANSUhEUgAAAHgAAAB4CAYAAAA5ZDbSAAAAAXNSR0IArs4c6QAACbVJREFUeF7tXAWoFVEQnW+',
+        mimetype: 'image/png',        
+      });
+    });
+  })
+  describe('resourceFromText', () => {
+    const uid = (Math.random() + 1).toString(36).substring(10);
+    const dataURL = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAHgAAAB4CAYAAAA5ZDbSAAAAAXNSR0IArs4c6QAACbVJREFUeF7tXAWoFVEQnW+'
+
+    it('Replace localhost to render.percy.local', () => {
+
+      Object.defineProperty(window.document, 'URL', {
+        writable: true,
+        value: 'http://localhost',
+      });
+      const result = resourceFromText(uid, 'image/png', dataURL);
+      expect(result).toEqual({
+        url: `http://render.percy.local/__serialized__/${uid}.png`,
+        content: dataURL,
+        mimetype: 'image/png',  
+      });
+    });
+
+    it('If URL is not localhost, return as is', () => {
+      Object.defineProperty(window.document, 'URL', {
+        writable: true,
+        value: 'http://example.com',
+      });
+      const result = resourceFromText(uid, 'image/png', dataURL);
+      expect(result).toEqual({
+        url: `http://example.com/__serialized__/${uid}.png`,
+        content: dataURL,
+        mimetype: 'image/png',        
+      });
+    });
+  })
 });
