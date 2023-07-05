850780d0abebf29a240575cac05d5eeae7ef62b2
amit3200 <tusharamit@yahoo.com>
2023-07-06 04:19:47 +0530

    Adding Start Stop Support for PoA

--- packages/webdriver-utils/src/providers/providerResolver.js
+++ packages/webdriver-utils/src/providers/providerResolver.js
@@ -2,9 +2,9 @@ import GenericProvider from './genericProvider.js';
 import AutomateProvider from './automateProvider.js';
 
 export default class ProviderResolver {
-  static resolve(sessionId, commandExecutorUrl, capabilities, sessionCapabilities, clientInfo, environmentInfo, options) {
+  static resolve(sessionId, commandExecutorUrl, capabilities, sessionCapabilities, clientInfo, environmentInfo, options, buildInfo) {
     // We can safely do [0] because GenericProvider is catch all
     const Klass = [AutomateProvider, GenericProvider].filter(x => x.supports(commandExecutorUrl))[0];
-    return new Klass(sessionId, commandExecutorUrl, capabilities, sessionCapabilities, clientInfo, environmentInfo, options);
+    return new Klass(sessionId, commandExecutorUrl, capabilities, sessionCapabilities, clientInfo, environmentInfo, options, buildInfo);
   }
 }
