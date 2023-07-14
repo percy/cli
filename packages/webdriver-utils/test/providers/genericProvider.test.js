c00fe09322aeeec9b098e08dc5aa69dae6a9a5ca
nilshah98 <neelshah.1998@gmail.com>
2023-07-14 15:34:05 +0530

    chore: lint fix

--- packages/webdriver-utils/test/providers/genericProvider.test.js
+++ packages/webdriver-utils/test/providers/genericProvider.test.js
@@ -2,8 +2,7 @@ import GenericProvider from '../../src/providers/genericProvider.js';
 import Driver from '../../src/driver.js';
 import MetaDataResolver from '../../src/metadata/metaDataResolver.js';
 import DesktopMetaData from '../../src/metadata/desktopMetaData.js';
-import utils from '@percy/sdk-utils';
-import Cache from '../../src/util/cache.js'
+import Cache from '../../src/util/cache.js';
 import MobileMetaData from '../../src/metadata/mobileMetaData.js';
 
 describe('GenericProvider', () => {
