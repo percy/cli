01c7e2c944e54cdd45e72996c73c20b9a1165bee
pankaj443 <pankajofficial443@gmail.com>
2023-08-28 10:37:19 +0530

    Use node 14

--- scripts/build.sh
+++ scripts/build.sh
@@ -77,14 +77,14 @@ npm run build_cjs
 
 cp -R ./build/* packages/
 
-pkg --targets node14-linux-x64,node14-macos-x64,node14-macos-arm64,node14-win-x64,node14-linux-arm64  ./packages/cli/bin/run.js -d
+# pkg ./packages/cli/bin/run.js -d
+pkg --targets node14-macos-arm64 ./packages/cli/bin/run.js -d
 
 ls
-mv run-macos-x64 percy-macos-x64
+mv run-macos percy-macos-x64
 mv run-macos-arm64 percy-macos-arm64
-mv run-linux-arm64 percy-linux-arm64
-mv run-linux-x64 percy-linux-x64
-mv run-win-x64.exe percy-win-x64.exe
+mv run-linux percy-linux-x64
+mv run-win.exe percy-win.exe
 
 # cleanup
 rm -rf temp
@@ -92,26 +92,26 @@ rm -rf build
 
 # Sign & Notrize mac app
 
-echo "$APPLE_DEV_CERT" | base64 -d > AppleDevIDApp.p12
+# echo "$APPLE_DEV_CERT" | base64 -d > AppleDevIDApp.p12
 
-security create-keychain -p percy percy.keychain
-security import AppleDevIDApp.p12 -t agg -k percy.keychain -P ChaiTime -A
-security list-keychains -s ~/Library/Keychains/percy.keychain
-security default-keychain -s ~/Library/Keychains/percy.keychain
-security unlock-keychain -p "percy" ~/Library/Keychains/percy.keychain
-security set-keychain-settings -t 3600 -l ~/Library/Keychains/percy.keychain
-security set-key-partition-list -S apple-tool:,apple:,codesign: -s -k percy ~/Library/Keychains/percy.keychain-db
+# security create-keychain -p percy percy.keychain
+# security import AppleDevIDApp.p12 -t agg -k percy.keychain -P ChaiTime -A
+# security list-keychains -s ~/Library/Keychains/percy.keychain
+# security default-keychain -s ~/Library/Keychains/percy.keychain
+# security unlock-keychain -p "percy" ~/Library/Keychains/percy.keychain
+# security set-keychain-settings -t 3600 -l ~/Library/Keychains/percy.keychain
+# security set-key-partition-list -S apple-tool:,apple:,codesign: -s -k percy ~/Library/Keychains/percy.keychain-db
 
-codesign  --force --verbose=4 --deep -s "Developer ID Application: BrowserStack Inc (763K6K6H44)" --options runtime --keychain ~/Library/Keychains/percy.keychain percy-macos-arm64
-codesign  --force --verbose=4 --deep -s "Developer ID Application: BrowserStack Inc (763K6K6H44)" --options runtime --keychain ~/Library/Keychains/percy.keychain percy-macos-x64
+# codesign  --force --verbose=4 --deep -s "Developer ID Application: BrowserStack Inc (763K6K6H44)" --options runtime --keychain ~/Library/Keychains/percy.keychain percy-macos-arm64
+# codesign  --force --verbose=4 --deep -s "Developer ID Application: BrowserStack Inc (763K6K6H44)" --options runtime --keychain ~/Library/Keychains/percy.keychain percy-macos-x64
 
-zip percy-macos-arm64.zip percy-macos-arm64
-zip percy-macos-x64.zip percy-macos-x64
+# zip percy-macos-arm64.zip percy-macos-arm64
+# zip percy-macos-x64.zip percy-macos-x64
 
-cat scripts/notarize_config.json.tmpl | sed -e "s/{{APPLE_ID_USERNAME}}/$APPLE_ID_USERNAME/" | sed -e "s/{{APPLE_ID_KEY}}/$APPLE_ID_KEY/" | sed -e "s/{{ZIP}}/percy-macos-x64.zip/" | sed -e "s/{{BUNDLE_ID}}/com.percy.io.intel/" > notarize_config_intel.json
-cat scripts/notarize_config.json.tmpl | sed -e "s/{{APPLE_ID_USERNAME}}/$APPLE_ID_USERNAME/" | sed -e "s/{{APPLE_ID_KEY}}/$APPLE_ID_KEY/" | sed -e "s/{{ZIP}}/percy-macos-arm64.zip/" | sed -e "s/{{BUNDLE_ID}}/com.percy.io.arm/" > notarize_config_arm.json
+# cat scripts/notarize_config.json.tmpl | sed -e "s/{{APPLE_ID_USERNAME}}/$APPLE_ID_USERNAME/" | sed -e "s/{{APPLE_ID_KEY}}/$APPLE_ID_KEY/" | sed -e "s/{{ZIP}}/percy-macos-x64.zip/" | sed -e "s/{{BUNDLE_ID}}/com.percy.io.intel/" > notarize_config_intel.json
+# cat scripts/notarize_config.json.tmpl | sed -e "s/{{APPLE_ID_USERNAME}}/$APPLE_ID_USERNAME/" | sed -e "s/{{APPLE_ID_KEY}}/$APPLE_ID_KEY/" | sed -e "s/{{ZIP}}/percy-macos-arm64.zip/" | sed -e "s/{{BUNDLE_ID}}/com.percy.io.arm/" > notarize_config_arm.json
 
-gon -log-level=info -log-json notarize_config_intel.json
-gon -log-level=info -log-json notarize_config_arm.json
+# gon -log-level=info -log-json notarize_config_intel.json
+# gon -log-level=info -log-json notarize_config_arm.json
 
-security delete-keychain percy.keychain
+# security delete-keychain percy.keychain
