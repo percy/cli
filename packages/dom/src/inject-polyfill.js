bec62e09f1393eea652e165eec2d3575d029f43c
pankaj443 <pankajofficial443@gmail.com>
2024-04-24 17:14:48 +0530

    Fallback to clone if head node is missing

--- packages/dom/src/inject-polyfill.js
+++ packages/dom/src/inject-polyfill.js
@@ -36,7 +36,7 @@ export function injectDeclarativeShadowDOMPolyfill(ctx) {
   `.replace(/(\n|\s{2}|\t)/g, '');
 
   // run polyfill as first thing post dom content is loaded
-  clone.prepend(scriptEl);
+  (clone.head ?? clone).prepend();
 }
 
 export default injectDeclarativeShadowDOMPolyfill;
