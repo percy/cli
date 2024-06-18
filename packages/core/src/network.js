090485da3e387793da51f03577d03d9f917c37cd
this-is-shivamsingh <shivam.si@browserstack.com>
2024-06-18 20:19:41 +0530

    chore: remove async from network.js

--- packages/core/src/network.js
+++ packages/core/src/network.js
@@ -129,7 +129,7 @@ export class Network {
   }
 
   // Throw a better network timeout error
-  async _throwTimeoutError(msg, filter = () => true) {
+  _throwTimeoutError(msg, filter = () => true) {
     if (this.log.shouldLog('debug')) {
       let reqs = Array.from(this.#requests.values()).filter(filter).map(r => r.url);
       msg += `\n\n  ${['Active requests:', ...reqs].join('\n  - ')}\n`;
