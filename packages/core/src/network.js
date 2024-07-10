697657b1927dbc9f3793ebbf50c93fe6a2c146e2
chinmay-browserstack <92926953+chinmay-browserstack@users.noreply.github.com>
2024-07-10 17:46:55 +0530

    Update packages/core/src/network.js
    Co-authored-by: Pradum Kumar <pradumraj360@gmail.com>

--- packages/core/src/network.js
+++ packages/core/src/network.js
@@ -112,7 +112,7 @@ export class Network {
     const activeRequests = this.getActiveRequests(filter);
     /* istanbul ignore if: race condition, very hard to mock this */
     if (activeRequests.length > 0) {
-      this.log.info(`There are some active requests while asset discovery try increasing networkIdleTimeout. \n ${activeRequests}`);
+      this.log.info(`There are ${activeRequests.length} active requests pending during asset discovery. Try increasing the networkIdleTimeout to resolve this issue. \n ${activeRequests}`);
     }
   }
 
