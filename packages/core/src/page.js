7fd1392939cbd1f918350caed1c9601dd14163b9
Chinmay Maheshwari <chinmay.m@browserstack.com>
2024-07-11 17:29:25 +0530

    Refactored according to comments

--- packages/core/src/page.js
+++ packages/core/src/page.js
@@ -48,36 +48,34 @@ export class Page {
     });
   }
 
+  mergeCookies(userPassedCookie, autoCapturedCookie) {
+    if (!autoCapturedCookie) return userPassedCookie;
+    if (userPassedCookie.length === 0) return autoCapturedCookie;
+
+    const mergedCookies = [...userPassedCookie, ...autoCapturedCookie];
+    const uniqueCookies = [];
+    const names = new Set();
+
+    for (const cookie of mergedCookies) {
+      if (!names.has(cookie.name)) {
+        uniqueCookies.push(cookie);
+        names.add(cookie.name);
+      }
+    }
+
+    return uniqueCookies;
+  }
+
   // Go to a URL and wait for navigation to occur
   async goto(url, { waitUntil = 'load', cookies } = {}) {
     this.log.debug(`Navigate to: ${url}`, this.meta);
 
     let navigate = async () => {
-      const mergeCookies = (userPassedCookie, autoCapturedCookie) => {
-        const mergedCookies = [...userPassedCookie, ...autoCapturedCookie];
-        const uniqueCookies = [];
-        const names = new Set();
-
-        for (const cookie of mergedCookies) {
-          if (!names.has(cookie.name)) {
-            uniqueCookies.push(cookie);
-            names.add(cookie.name);
-          }
-        }
-
-        return uniqueCookies;
-      };
-
       const userPassedCookie = this.session.browser.cookies;
       // set cookies before navigation so we can default the domain to this hostname
       if (userPassedCookie.length || cookies) {
         let defaultDomain = hostname(url);
-
-        if (userPassedCookie.length > 0 && cookies) {
-          cookies = mergeCookies(userPassedCookie, cookies);
-        } else if (userPassedCookie.length) {
-          cookies = userPassedCookie;
-        }
+        cookies = this.mergeCookies(userPassedCookie, cookies);
 
         await this.session.send('Network.setCookies', {
           // spread is used to make a shallow copy of the cookie
