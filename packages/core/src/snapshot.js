400ff46982e980b13d20786436042ffcd8a359e8
this-is-shivamsingh <shivam.si@browserstack.com>
2024-07-22 04:37:23 +0530

    chore: lint fix

--- packages/core/src/snapshot.js
+++ packages/core/src/snapshot.js
@@ -102,7 +102,7 @@ function mapSnapshotOptions(snapshots, context) {
     // transform snapshot URL shorthand into an object
     if (typeof snapshot === 'string') snapshot = { url: snapshot };
 
-    validateAndFixSnapshotUrl(snapshot)
+    validateAndFixSnapshotUrl(snapshot);
 
     let url = validURL(snapshot.url, context?.baseUrl);
     // normalize the snapshot url and use it for the default name
