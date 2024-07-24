a5554279feaad46a140f410d3ed91ed454c88918
Shivam singh <140813296+this-is-shivamsingh@users.noreply.github.com>
2024-07-24 18:07:06 +0530

    Detect and modify invalid URL characters (#1664)
    * feat: added invalid URL detection for snapshot & network urls
    
    * test: added test for snapshot urls for detecting snapshot url
    
    * test: added test for network urls
    
    * test: test fix
    
    * test: snapshot.test.js fix
    
    * test: coverage fix
    
    * test: coverage fix
    
    * fix: addressing comment
    feat: moved logging and trycatch handling to utils.js
    chore: Updated error/warning message for invalid URL's
    
    * test: test fix on assets discovery
    
    * test: added test for utils.js(decodeAndEncodeURLWithLogging)
    
    * chore: addressing comments, moving to function validateAndFixSnapshotUrl in snapshot.js
    
    * chore: lint fix

--- packages/core/src/snapshot.js
+++ packages/core/src/snapshot.js
@@ -7,7 +7,8 @@ import {
   request,
   hostnameMatches,
   yieldTo,
-  snapshotLogName
+  snapshotLogName,
+  decodeAndEncodeURLWithLogging
 } from './utils.js';
 import { JobData } from './wait-for-job.js';
 
@@ -21,6 +22,21 @@ function validURL(url, base) {
     return new URL(url, base);
   } catch (e) {
     throw new Error(`Invalid snapshot URL: ${e.input}`);
+  }
+}
+
+function validateAndFixSnapshotUrl(snapshot) {
+  let log = logger('core:snapshot');
+  // encoding snapshot url, if contians invalid URI characters/syntax
+  let modifiedURL = decodeAndEncodeURLWithLogging(snapshot.url, log, {
+    meta: { snapshot: { name: snapshot.name || snapshot.url } },
+    shouldLogWarning: true,
+    warningMessage: `Invalid URL detected for url: ${snapshot.url} - the snapshot may fail on Percy. Please confirm that your website URL is valid.`
+  });
+
+  if (modifiedURL !== snapshot.url) {
+    log.debug(`Snapshot URL modified to: ${modifiedURL}`);
+    snapshot.url = modifiedURL;
   }
 }
 
@@ -86,8 +102,10 @@ function mapSnapshotOptions(snapshots, context) {
     // transform snapshot URL shorthand into an object
     if (typeof snapshot === 'string') snapshot = { url: snapshot };
 
-    // normalize the snapshot url and use it for the default name
+    validateAndFixSnapshotUrl(snapshot);
+
     let url = validURL(snapshot.url, context?.baseUrl);
+    // normalize the snapshot url and use it for the default name
     snapshot.name ||= `${url.pathname}${url.search}${url.hash}`;
     snapshot.url = url.href;
 
