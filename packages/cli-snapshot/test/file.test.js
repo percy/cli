159e0ddb0dca5430d507044609178ac0f9254ccb
Mukul Bhutiani <mukul.b@browserstack.com>
2024-12-20 00:51:31 +0530

    cli-snapshot-fix

--- packages/cli-snapshot/test/file.test.js
+++ packages/cli-snapshot/test/file.test.js
@@ -8,6 +8,7 @@ describe('percy snapshot <file>', () => {
   beforeEach(async () => {
     snapshot.packageInformation = { name: '@percy/cli-snapshot' };
     process.env.PERCY_TOKEN = '<<PERCY_TOKEN>>';
+    process.env.PERCY_FORCE_PKG_VALUE = JSON.stringify({ "name": "@percy/client", "version": "1.0.0" });
 
     server = await createTestServer({
       default: () => [200, 'text/html', '<p>Test</p>']
@@ -44,6 +45,7 @@ describe('percy snapshot <file>', () => {
 
   afterEach(async () => {
     delete process.env.PERCY_TOKEN;
+    delete process.env.PERCY_FORCE_PKG_VALUE
     delete process.env.PERCY_CLIENT_ERROR_LOGS;
     delete snapshot.packageInformation;
     await server.close();
