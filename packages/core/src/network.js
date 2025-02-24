d615f5a2ab8db9ad29f1525435bb0f9d9276ca31
Shivanshu Singh <shivanshu.si@browserstack.com>
2025-02-24 17:48:06 +0530

    fixed content length NaN (#1866)
    * fixed content length NaN
    
    * Added log for NaN content length
    
    * Added check for headerless response
    
    * addressed comments
    
    * minor fix
    
    * minor fix
    
    * Added test cases for content length casing
    
    * addressed comments

--- packages/core/src/network.js
+++ packages/core/src/network.js
@@ -470,7 +470,8 @@ async function saveResponseResource(network, request, session) {
   // Checing for content length more than 100MB, to prevent websocket error which is governed by
   // maxPayload option of websocket defaulted to 100MB.
   // If content-length is more than our allowed 25MB, no need to process that resouce we can return log.
-  let contentLength = parseInt(response.headers['Content-Length']);
+  let contentLength = response.headers?.[Object.keys(response.headers).find(key => key.toLowerCase() === 'content-length')];
+  contentLength = parseInt(contentLength);
   if (contentLength > MAX_RESOURCE_SIZE) {
     return log.debug('- Skipping resource larger than 25MB', meta);
   }
@@ -491,6 +492,9 @@ async function saveResponseResource(network, request, session) {
         return log.debug('- Skipping remote resource', meta);
       } else if (!body.length) {
         return log.debug('- Skipping empty response', meta);
+      } else if (body.length > MAX_RESOURCE_SIZE) {
+        log.debug('- Missing headers for the requested resource.', meta);
+        return log.debug('- Skipping resource larger than 25MB', meta);
       } else if (!ALLOWED_STATUSES.includes(response.status)) {
         return log.debug(`- Skipping disallowed status [${response.status}]`, meta);
       } else if (!enableJavaScript && !ALLOWED_RESOURCES.includes(request.type)) {
