e7edbb1f22b1804a5434603fba6e8a48825f896f
bhokaremoin <bhokaremoin@gmail.com>
2025-07-15 18:03:55 +0530

    adding command in build

--- packages/cli-build/src/build.js
+++ packages/cli-build/src/build.js
@@ -3,10 +3,12 @@ import finalize from './finalize.js';
 import wait from './wait.js';
 import id from './id.js';
 import approve from './approve.js';
+import reject from './reject.js';
+import unapprove from './unapprove.js';
 
 export const build = command('build', {
   description: 'Finalize and wait on Percy builds',
-  commands: [finalize, wait, id, approve]
+  commands: [finalize, wait, id, approve, unapprove, reject],
 });
 
 export default build;
